0b89dec00b8ff134a385dad003bbe368a67b40e2
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-04-21 20:54:56 -0500

    added routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ListingComponent } from './listing/listing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    ListingComponent
+    ListingComponent,
+    ProfileComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
