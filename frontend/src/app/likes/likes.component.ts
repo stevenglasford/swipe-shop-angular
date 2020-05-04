f5bfd0c3194d1db7e98deda5eb070571004c106d
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-04 16:16:22 -0500

    likes works

--- frontend/src/app/likes/likes.component.ts
+++ frontend/src/app/likes/likes.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ListingObject } from '../listing-object';
 import { DataInteractionService } from '../data-interaction.service';
+import { CookieService } from 'ngx-cookie-service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { UserProfile } from '../user-profile';
 
 @Component({
   selector: 'app-likes',
@@ -10,12 +14,48 @@ import { DataInteractionService } from '../data-interaction.service';
 export class LikesComponent implements OnInit {
 
   // dataInteractionService = new DataInteractionService();
-  listingObjects : ListingObject[];
+  listingObjects : ListingObject[] = [];
 
-  constructor(private dataInteractionService : DataInteractionService) { }
+  constructor(private router : Router, private cookieService : CookieService, private db : AngularFirestore, private dataInteractionService : DataInteractionService) { }
 
   ngOnInit(): void {
-    this.listingObjects = this.dataInteractionService.pullLikes();
-  }
+    // this.listingObjects = this.dataInteractionService.pullLikes();
+    this.db.collection<any>('views').ref.where('username', '==', this.cookieService.get('username'))
+        .get()
+        .then(res => {
+          if (res.docs.length == 0){
+
+          } else {
+            res.forEach(view => {
+              if (view.data()['liked']){
+                let docId = view.data()['docId']
+
+                this.db.collection<any>('products').ref.get().then(res => {
+                  res.forEach(product => {
+                    if (product.id == docId){
+                      var listingObj = new ListingObject(product.data()['price'], product.data()['location'], product.data()['name'], product.data()['photoUrl'], null)
+
+                      this.db.collection<any>('users').ref.where('username', '==', product.data()['owner']).get().then(res => {
+                        res.forEach(profile => {
+                          listingObj.userProfile = new UserProfile(profile.data()['userId'], profile.data()['username'], profile.data()['profilePictureUrl'], profile.data()['location'], [])
+                        })
+
+                        this.listingObjects.push(listingObj);
+                      })
+
+                      
+
+                      
+
+                    }
+                  })
+                })
+
+              }
+           })
+          } 
+      })
+    }
+
 
 }
