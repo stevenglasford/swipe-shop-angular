cb8a0650da7ba72f53c75a069482ef579f5a769a
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-04 16:43:32 -0500

    messages firebase

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataInteractionService } from '../data-interaction.service';
 import { Message } from '../message';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { CookieService } from 'ngx-cookie-service';
+import { UserProfile } from '../user-profile';
 
 @Component({
   selector: 'app-messages',
@@ -9,14 +12,48 @@ import { Message } from '../message';
 })
 export class MessagesComponent implements OnInit {
 
-  // dataInteractionService = new DataInteractionService();
+  messagePreviews : Message[] = [];
 
-  messagePreviews : Message[];
-
-  constructor(private dataInteractionService : DataInteractionService) { }
+  constructor(private db : AngularFirestore, private cookieService : CookieService, private dataInteractionService : DataInteractionService) { }
 
   ngOnInit(): void {
-    this.messagePreviews = this.dataInteractionService.pullMessagePreviews();
+    var allMessages : Message[] = [];
+
+    this.db.collection<any>('messages').ref.get().then(res => {
+      res.forEach(message => {
+
+        var userTo = null;
+        this.db.collection<any>('users').ref.where('username', '==', message.data()['userTo']).get().then(res => {
+          res.forEach(user => {
+            userTo = new UserProfile(user.data()['userId'], name, user.data()['profilePictureUrl'], user.data()['location'], [])
+          })
+
+          var userFrom = null;
+          this.db.collection<any>('users').ref.where('username', '==', message.data()['userFrom']).get().then(res => {
+            res.forEach(user => {
+              userFrom = new UserProfile(user.data()['userId'], name, user.data()['profilePictureUrl'], user.data()['location'], [])
+            })
+
+            
+            allMessages.push(new Message(userTo, userFrom, userFrom.location, new Date((message.data()['time'])), message.data()['attachmentUrl']))
+
+            // now filter to what we want
+
+            this.messagePreviews = allMessages;
+
+
+          });
+        });
+      })
+    });
+  }
+
+  userProfileFromUsername(name : string){
+    this.db.collection<any>('users').ref.where('username', '==', name).get().then(res => {
+      res.forEach(user => {
+        return new UserProfile(user.data()['userId'], name, user.data()['profilePictureUrl'], user.data()['location'], [])
+      })
+    });
   }
 
 }
